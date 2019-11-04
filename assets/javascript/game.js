8395d9114321fe3b45e7a944229d0932949bfcbe
Casey Ferguson <cferg019@gmail.com>
2019-11-04 18:29:14 -0500

    all elements added - no functionality yet

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+// variables 
+var randomNum = 0;
+var scoreCount = 0;
+var gemStone1 = 0;
+var gemStone2 = 0;
+var gemStone3 = 0;
+var gemStone4 = 0;
+var wins = 0;
+var losses = 0;
+
+var randomNumText = document.getElementById("randomNum")
+var scoreCountText = document.getElementById("scoreCount")
+var winsText = document.getElementById("wins")
+var lossesText = document.getElementById("losses")
