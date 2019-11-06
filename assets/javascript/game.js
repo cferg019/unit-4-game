85c06f97faf34749e5318e989d263a4cdf72b559
Casey Ferguson <cferg019@gmail.com>
2019-11-05 20:43:34 -0500

    started functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,31 +13,45 @@ var scoreCountText = document.getElementById("scoreCount")
 var winsText = document.getElementById("wins")
 var lossesText = document.getElementById("losses")
 
-
+// screen refresh
+var updateScreen = function() {
+    randomNumText.textContent = "Random Number: " + randomNum;
+    winsText.textContent = "Wins: " + wins;
+    lossesText.textContent = "Losses: " + losses;
+    scoreCountText.textContent = "Score: " + scoreCount;
+}
 
 // game start 
-randomNum = [Math.floor(Math.random() * 101) + 19];
-gemStone1 = [Math.floor(Math.random() * 12)];
-gemStone2 = [Math.floor(Math.random() * 12)];
-gemStone3 = [Math.floor(Math.random() * 12)];
-gemStone4 = [Math.floor(Math.random() * 12)];
+var startUp = function() {
+    randomNum = [Math.floor(Math.random() * 101) + 19];
+    gemStone1 = [Math.floor(Math.random() * 12) + 1];
+    gemStone2 = [Math.floor(Math.random() * 12) + 1];
+    gemStone3 = [Math.floor(Math.random() * 12) + 1];
+    gemStone4 = [Math.floor(Math.random() * 12) + 1];
+    
+    updateScreen()
+}
 
 // gemstone 1
-$("#gemStone1").on("click", function() {
+$("#gemStone1").on("click", function () {
     console.log(gemStone1)
 })
 
 // gemstone 2
-$("#gemStone2").on("click", function() {
+$("#gemStone2").on("click", function () {
     console.log(gemStone2)
 })
 
 // gemstone 3
-$("#gemStone3").on("click", function() {
+$("#gemStone3").on("click", function () {
     console.log(gemStone3)
 })
 
 // gemstone 4
-$("#gemStone4").on("click", function() {
+$("#gemStone4").on("click", function () {
     console.log(gemStone4)
 })
+
+
+
+startUp()
