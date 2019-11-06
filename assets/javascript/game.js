5e6b9d73fa811c90f0e7bf1657330a6cf7cf8eb8
Casey Ferguson <cferg019@gmail.com>
2019-11-05 19:58:02 -0500

    random numbers generated when clicking gems & random goal number generated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,3 +12,32 @@ var randomNumText = document.getElementById("randomNum")
 var scoreCountText = document.getElementById("scoreCount")
 var winsText = document.getElementById("wins")
 var lossesText = document.getElementById("losses")
+
+
+
+// game start 
+randomNum = [Math.floor(Math.random() * 101) + 19];
+gemStone1 = [Math.floor(Math.random() * 12)];
+gemStone2 = [Math.floor(Math.random() * 12)];
+gemStone3 = [Math.floor(Math.random() * 12)];
+gemStone4 = [Math.floor(Math.random() * 12)];
+
+// gemstone 1
+$("#gemStone1").on("click", function() {
+    console.log(gemStone1)
+})
+
+// gemstone 2
+$("#gemStone2").on("click", function() {
+    console.log(gemStone2)
+})
+
+// gemstone 3
+$("#gemStone3").on("click", function() {
+    console.log(gemStone3)
+})
+
+// gemstone 4
+$("#gemStone4").on("click", function() {
+    console.log(gemStone4)
+})
